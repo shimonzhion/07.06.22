4fa10130fc74ebf729846ddb2845e3b57f9f0cf7
shimonzhion <brys94204@gmail.com>
2022-06-08 09:13:28 +0300

    08.06.22 timers

--- main.js
+++ main.js
@@ -9,17 +9,16 @@
 // function printCounter(){
 //     setInterval(()=>{
 //         text2.innerHTML += counter++ +"<br>"
-       
-        
+
 //     },6000)
 
 // }
 // printCounter()
-var IntervalId ;
+var IntervalId;
 // function printI_Love_Timers(){
 //   IntervalId =  setInterval(()=>{
 //         text3.innerHTML += "I love timers <br>"
-  
+
 //     },5000)
 // }
 
@@ -29,9 +28,7 @@ var IntervalId ;
 //     clearInterval(IntervalId)
 // }
 
-
 // function printDate (){
-
 
 //    IntervalId = setInterval(()=>{
 //     var d = new Date();
@@ -39,32 +36,59 @@ var IntervalId ;
 //     var m = d.getMinutes();
 //     var h = d.getHours();
 //         text3.innerHTML += ("0" + h).substr(-2) +":" +("0" + m).substr(-2) +":" +("0" + s).substr(-2) +"<br>";
-    
+
 //     },1000)
- 
+
 // }
 // printDate ()
- function min () {
-    var minute = min_input.value -1;
-    var sec = 59;
-    setInterval(function() {
-        if (minute < 0){
-            return text5.innerHTML= "game over"
-        }
+
+// function min() {
+//   var minute = min_input.value - 1;
+//   var sec = 59;
+
+//   IntervalId = setInterval(function () {
+//     if (minute < 0) {
+//       return (text5.innerHTML = "game over");
+//     }
+
+//     text5.innerHTML = minute + " : " + sec;
+//     sec--;
+//     if (sec == 0) {
+//       minute--;
+//       sec = 59;
+//     }
+//   }, 1000);
+// }
+
+// function stop() {
+//   clearInterval(IntervalId);
+// }
 
 
-         text5.innerHTML = minute + " : " + sec;
-      sec--;
-    
-     
-  
-        if(sec==0){
-   
-        minute --
-        sec=59
-        }
-     
-    }, 1000);
-    
+function somfunc (){
+  var usertime =   Alarm_Clock.value
+  var h = parseInt(usertime.substring(0,3))
+ var m = parseInt(usertime.substr(3,2))
+ var d = new Date()
+  var now_h=  d.getHours()
+  var now_m = parseInt(d.getMinutes())
+  var z;
+  var w;
+ 
+  if (h>=now_h){
+   z= h-now_h
   }
+
+  if(now_h>=h||now_h==h){
+   z=  now_h-h
+  }
+  if(m>=now_m){
+    w = m-now_m
+   }
+   if(now_m>=m){
+    w =now_m-m
+   }
+ 
+  console.log(z+":"+w)
+}
 
